2a451d3a25534695e01e4d0104502b9e5880e959
lexis-webb <lexiswebb.design@gmail.com>
2025-01-29 01:09:24 -0500

    new/ added header

---
+++ ShoppingCart/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Cart from './components/Cart/Cart'
+import Header from './components/Header/Header'
+import Products from './components/Products/Products'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Header />
+      <Products />
+      <Cart />
+    </div>
+  )
+}
+
+export default App
---
+++ ShoppingCart/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Header.scss";
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='container'>
+        <div className='left'>
+          <h2>Cart</h2>
+        </div>
+          <div className='right'>
+            <i className='fas fa-count-plus'></i>
+            <div className='count'></div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
